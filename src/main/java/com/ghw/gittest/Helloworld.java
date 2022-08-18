7b32a2aae8f80c469123ce0507a14e51e44c4536
GHW <Iron-ghw@sjtu.edu.cn>
2022-08-18 10:45:08 +0800

    my idea second git commit,another_branch

--- src/main/java/com/ghw/gittest/Helloworld.java
+++ src/main/java/com/ghw/gittest/Helloworld.java
@@ -7,5 +7,6 @@ package com.ghw.gittest;
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello another_branch");
     }
 }
