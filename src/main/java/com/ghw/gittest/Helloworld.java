1744ef2232903d9843d5f95284d23c8f05f89081
GHW <Iron-ghw@sjtu.edu.cn>
2022-08-18 10:34:32 +0800

    my idea first git commit

---
+++ src/main/java/com/ghw/gittest/Helloworld.java
@@ -0,0 +1,11 @@
+package com.ghw.gittest;
+
+/**
+ * @author ghw
+ * @create 2022-08-1810:28
+ */
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
